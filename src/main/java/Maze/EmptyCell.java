782b05251759b2e5f6dc99c76c62b48074c72095
Loris <crullloris@gmail.com>
2019-05-15 15:47:01 +0200

    Création des cellules qui composent les niveaux

---
+++ src/main/java/Maze/EmptyCell.java
@@ -0,0 +1,13 @@
+package Maze;
+
+public class EmptyCell extends Cell {
+
+    private EmptyCell(EmptyCell rc) {
+        super(rc);
+    }
+
+    @Override
+    public Cell clone() {
+        return new EmptyCell(this);
+    }
+}
