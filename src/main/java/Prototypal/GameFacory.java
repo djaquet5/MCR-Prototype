e67fa1c43f13f3a69ad3c83bdc195e7489b30c9d
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-05-15 16:22:19 +0200

    Oublié d'ajouter l'ether dans la factory

--- src/main/java/Prototypal/GameFacory.java
+++ src/main/java/Prototypal/GameFacory.java
@@ -1,22 +1,28 @@
 package Prototypal;
 
-import Maze.Cell;
 import Maze.ReachableCell;
-import Stuff.Item;
-import Stuff.Potion;
+import Stuff.*;
 
 public class GameFacory{
     private Potion prototypePotion;
+    private Ether prototypeEther;
 
 
     public GameFacory() {
         prototypePotion = new Potion();
+        prototypeEther = new Ether();
     }
 
     public Item MakePotion(ReachableCell cell) {
         Potion p = (Potion) prototypePotion.clone();
         p.initialize(cell);
         return p;
+    }
+
+    public Item MakeEther(ReachableCell cell){
+        Ether e = (Ether) prototypeEther.clone();
+        e.initialize(cell);
+        return e;
     }
 
 }
