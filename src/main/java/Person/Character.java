6cb307b91086c5466b7016f2c90438db8e3c16cb
yoann0000 <47739072+yoann0000@users.noreply.github.com>
2019-05-15 15:35:07 +0200

    chara update

--- src/main/java/Person/Character.java
+++ src/main/java/Person/Character.java
@@ -1,6 +1,12 @@
 package Person;
 
 import Magic.Spell;
+import Stuff.Item;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public abstract class Character {
     private int hp;
@@ -12,6 +18,8 @@ public abstract class Character {
     private int magic;
     private int magicDefence;
     private String displayImage;
+    private List<Spell> spellSlots;
+    private Map<Item, Integer> inventory;
 
     public Character(int hp, int mp, int attack, int defence, int magic, int magicDefence, String displayImage){
         this.hp = this.maxHp = hp;
@@ -21,6 +29,9 @@ public abstract class Character {
         this.magic = magic;
         this.magicDefence = magicDefence;
         this.displayImage = displayImage;
+
+        spellSlots = new ArrayList<Spell>();
+        inventory = new HashMap<Item, Integer>();
     }
 
     public void setHp(int i){
@@ -63,6 +74,29 @@ public abstract class Character {
         return hp <= 0;
     }
 
+    public List<Spell> getSpellSlots() {
+        return spellSlots;
+    }
+
+    public void learnSpell(Spell spell) {
+        if (spellSlots.contains(spell))
+            System.out.println("Spell already known!");
+        else
+            spellSlots.add(spell);
+    }
+
+    public Map<Item, Integer> getInventory() {
+        return inventory;
+    }
+
+    private void addToinventory(Item item, int amount) {
+        if (inventory.containsKey(item)) {
+            inventory.put(item, inventory.get(item) + amount);
+        } else {
+            inventory.put(item, amount);
+        }
+    }
+
     public int attack(Character c){
         int damage = Math.max(this.attack + (int)(Math.random() * 13) - c.getDefence(), 1);
         c.setHp(damage * -1);
@@ -71,7 +105,8 @@ public abstract class Character {
 
     public int castMagic(Character target, Spell spell){
         if(this.mp < spell.getMpCost()){
-            throw new IllegalArgumentException("Not enough Mana!");
+            System.out.println("Not enough Mana!");
+            return 0;
         }
         int damage = Math.max((int)Math.floor(this.magic * spell.getPower() / 100) +
                 (int)(Math.random() * 13) - target.getMagicDefence(), 1);
