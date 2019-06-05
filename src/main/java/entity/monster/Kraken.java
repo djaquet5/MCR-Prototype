97065c927d7401a0bdc20cf4b16f8adcc162ac80
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-06-05 15:44:45 +0200

    Add spell to monsters

--- src/main/java/entity/monster/Kraken.java
+++ src/main/java/entity/monster/Kraken.java
@@ -1,11 +1,15 @@
 package entity.monster;
 
+import magic.Spell;
 import prototypal.Prototype;
 
 public class Kraken extends Monster {
 
     public Kraken() {
         super(330, 123, 220, 200, 99, 200, "PATH", 137);
+        spellSlots.add(new Spell("Frozzen Breath", 88, 20));
+        spellSlots.add(new Spell("Bubble Cramble", 33, 3));
+        spellSlots.add(new Spell("Death Anchor", 55, 10));
     }
 
     @Override
