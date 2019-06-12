1db0b4589db156db991c208a8bbd1769e1491c7a
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-06-12 12:04:00 +0200

    refactor intellij

--- src/main/java/entity/GameCharacter.java
+++ src/main/java/entity/GameCharacter.java
@@ -6,9 +6,7 @@ import prototypal.Prototype;
 import stuff.Item;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public abstract class GameCharacter implements Prototype {
     private int hp;
@@ -41,7 +39,7 @@ public abstract class GameCharacter implements Prototype {
         return maxHp;
     }
 
-    public void setMaxHp(int maxHp) {
+    protected void setMaxHp(int maxHp) {
         this.maxHp = maxHp;
     }
 
@@ -49,7 +47,7 @@ public abstract class GameCharacter implements Prototype {
         return maxMp;
     }
 
-    public void setMaxMp(int maxMp) {
+    protected void setMaxMp(int maxMp) {
         this.maxMp = maxMp;
     }
 
@@ -73,7 +71,7 @@ public abstract class GameCharacter implements Prototype {
         return attack;
     }
 
-    public int getDefence(){
+    protected int getDefence(){
         return defence;
     }
 
@@ -81,7 +79,7 @@ public abstract class GameCharacter implements Prototype {
         return magic;
     }
 
-    public int getMagicDefence(){
+    protected int getMagicDefence(){
         return magicDefence;
     }
 
@@ -90,11 +88,11 @@ public abstract class GameCharacter implements Prototype {
         return displayImagePath;
     }
 
-    public void setHp(int hp) {
+    protected void setHp(int hp) {
         this.hp = hp;
     }
 
-    public void setMp(int mp) {
+    protected void setMp(int mp) {
         this.mp = mp;
     }
 
@@ -102,7 +100,7 @@ public abstract class GameCharacter implements Prototype {
         this.attack = attack;
     }
 
-    public void setDefence(int defence) {
+    protected void setDefence(int defence) {
         this.defence = defence;
     }
 
@@ -110,7 +108,7 @@ public abstract class GameCharacter implements Prototype {
         this.magic = magic;
     }
 
-    public void setMagicDefence(int magicDefence) {
+    protected void setMagicDefence(int magicDefence) {
         this.magicDefence = magicDefence;
     }
 
@@ -129,13 +127,6 @@ public abstract class GameCharacter implements Prototype {
         this.position = position;
     }
 
-    public void learnSpell(Spell spell) {
-        if (spellSlots.contains(spell))
-            System.out.println("Spell already known!");
-        else
-            spellSlots.add(spell);
-    }
-
     public List<Item> getInventory() {
         return inventory;
     }
@@ -148,9 +139,7 @@ public abstract class GameCharacter implements Prototype {
     }
 
     public void useItem(Item item){
-        item.use(this);
-        System.out.println("ICI");
-        inventory.remove(item);
+        item.use(this);inventory.remove(item);
     }
 
     public int attack(GameCharacter c){
