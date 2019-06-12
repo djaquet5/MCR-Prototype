6b97a5cd81e6d58b9cb39ec577eb9a0965504d1f
RengokuryuuhonokaCrimsonFlame <julien.rod@heig-vd.ch>
2019-06-12 09:59:32 +0200

    Ecran game over ne fait pas planter le programme

--- src/main/java/display/GameOver.java
+++ src/main/java/display/GameOver.java
@@ -5,22 +5,24 @@ import javax.swing.plaf.metal.MetalComboBoxButton;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
-public class GameOver  implements KeyListener {
+public class GameOver {
     private JTextArea gameOverTextArea;
     private JPanel gameOverPanel;
 
-    private void createUIComponents() {}
+    public GameOver(){
+        gameOverPanel.addKeyListener(new KeyListener(){
+            @Override
+            public void keyTyped(KeyEvent keyEvent) {}
 
-    @Override
-    public void keyTyped(KeyEvent keyEvent) {}
+            @Override
+            public void keyPressed(KeyEvent keyEvent) {
+                Game.getInstance().setContentPane(new MainMenu().getMainPanel());
+            }
 
-    @Override
-    public void keyPressed(KeyEvent keyEvent) {
-        Game.getInstance().setContentPane(new MainMenu().getMainPanel());
+            @Override
+            public void keyReleased(KeyEvent keyEvent) {}
+        });
     }
-
-    @Override
-    public void keyReleased(KeyEvent keyEvent) {}
 
     public JPanel getGameOverPanel(){
         return gameOverPanel;
